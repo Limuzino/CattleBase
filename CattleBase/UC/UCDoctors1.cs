82989c347cdd0065ba0c58d0a52c6f9f552a3e4c
Ульянов Тимофей <ulyanovt@list.ru>
2022-05-08 09:12:32 +0500

    Сделал пока что тоже самое с UCDoctors

--- CattleBase/UC/UCDoctors1.cs
+++ CattleBase/UC/UCDoctors1.cs
@@ -7,14 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace CattleBase.UC
 {
     public partial class UCDoctors1 : UserControl
     {
+        static string connStr = "server=chuc.caseum.ru;port=33333;user=st_2_19_23;database=is_2_19_st23_KURS;password=12072300;";
+        MySqlConnection conn = new MySqlConnection(connStr);
+        private MySqlDataAdapter MyDA = new MySqlDataAdapter();
+        private BindingSource bSource = new BindingSource();
+        private DataSet ds = new DataSet();
+        private DataTable table = new DataTable();
+        string id_selected_rows = "0";
         public UCDoctors1()
         {
             InitializeComponent();
+        }
+        public void GetListUsers()
+        {
+            string sql = "SELECT * FROM doctor;";
+            conn.Open();
+            MyDA.SelectCommand = new MySqlCommand(sql, conn);
+            MyDA.Fill(table);
+            bSource.DataSource = table;
+            dataGridView1.DataSource = bSource;
+            conn.Close();
+        }
+
+        private void UCAnimals1_Load(object sender, EventArgs e)
+        {
+            GetListUsers();
+
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[1].Visible = true;
+            dataGridView1.Columns[2].Visible = true;
+            dataGridView1.Columns[3].Visible = true;
+            dataGridView1.Columns[4].Visible = true;
+
+            dataGridView1.Columns[1].ReadOnly = true;
+            dataGridView1.Columns[2].ReadOnly = true;
+            dataGridView1.Columns[3].ReadOnly = true;
+            dataGridView1.Columns[4].ReadOnly = true;
+
+            dataGridView1.RowHeadersVisible = false;
+
+            dataGridView1.ColumnHeadersVisible = true;
+
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            dataGridView1.Columns[1].HeaderText = "ФИО";
+            dataGridView1.Columns[2].HeaderText = "Поликлиника";
+            dataGridView1.Columns[3].HeaderText = "Номер телефона";
+            dataGridView1.Columns[4].HeaderText = "Почта";
+
+
+        }
+
+        public void GetSelectedIDString()
+        {
+            string index_selected_rows;
+            index_selected_rows = dataGridView1.SelectedCells[0].RowIndex.ToString();
+            id_selected_rows = dataGridView1.Rows[Convert.ToInt32(index_selected_rows)].Cells[0].Value.ToString();
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, e.RowIndex];
+            dataGridView1.CurrentRow.Selected = true;
+            GetSelectedIDString();
+        }
+
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (!e.RowIndex.Equals(-1) && !e.ColumnIndex.Equals(-1) && e.Button.Equals(MouseButtons.Right))
+            {
+                dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, e.RowIndex];
+                dataGridView1.CurrentCell.Selected = true;
+                GetSelectedIDString();
+            }
         }
     }
 }
